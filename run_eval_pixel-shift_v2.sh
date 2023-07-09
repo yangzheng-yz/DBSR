b06c3a5bfd19a07ea4ba00f73da40b6b09440663
zyzhengyang <yangzhengtj@outlook.com>
2023-07-09 01:19:10 -0400

    common update

--- run_eval_pixel-shift_v2.sh
+++ run_eval_pixel-shift_v2.sh
@@ -1,9 +1,9 @@
 CUDA_VISIBLE_DEVICES=1 \
 python evaluate_pixel-shift_v2.py \
---ckpt_path /mnt/samsung/zheng/downloaded_datasets/NightCity_1024x512/training_results/checkpoints/dbsr/model-5_synthetic/DBSRNet_ep0100.pth.tar \
+--ckpt_path /mnt/samsung/zheng/downloaded_datasets/NightCity_1024x512/training_results/checkpoints/dbsr/model-14_synthetic/DBSRNet_ep0100.pth.tar \
 --dataset_path /mnt/samsung/zheng/downloaded_datasets/NightCity_1024x512 \
---trajectory_path /home/yutong/zheng/projects/DBSR/util_scripts/trajectory_step-8_range-4.pkl \
---save_path /mnt/samsung/zheng/downloaded_datasets/NightCity_1024x512/evaluation_results/model-5_synthetic \
+--trajectory_path /home/yutong/zheng/projects/DBSR/util_scripts/trajectory_step-4_range-4.pkl \
+--save_path /mnt/samsung/zheng/downloaded_datasets/NightCity_1024x512/evaluation_results/model-14_synthetic \
 --cfg_file /home/yutong/zheng/projects/DBSR/configs/nightCity_eval.yaml \
---specify_trajetory_num 1 \
+--specify_trajetory_num 1000 \
 --save_results
